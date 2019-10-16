2d7bf6c25a16313f0669a91e935570877193490b
Jojo <katjajohanna@outlook.com>
2019-10-16 14:43:16 +0300

    Add new lambda for fetching hours

---
+++ hours/get-handler.js
@@ -0,0 +1,25 @@
+﻿// const hourService = require('./service')
+
+module.exports.handler = ((event, context, callback) => {
+    console.log(event)
+
+    callback(null, {
+        "statusCode": 200,
+        "headers": {},
+        "body": JSON.stringify({})
+    })
+
+    // hourService.getAll()
+    // .then(hours => {
+    //     console.log('Got hours', hours)
+    //     callback(null, {
+    //         "statusCode": 200,
+    //         "headers": {},
+    //         "body": JSON.stringify(hours)
+    //     })
+    // })
+    // .catch(err => {
+    //     console.log('Error', err)
+    //     callback(err)
+    // })
+});
