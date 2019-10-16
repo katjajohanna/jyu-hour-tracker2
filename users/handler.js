94442bdcfceb50c4c1e26973db81a0ff78a02759
Jojo <katjajohanna@outlook.com>
2019-10-16 09:31:45 +0300

    Add one lambda which returns empty object

---
+++ users/handler.js
@@ -0,0 +1,12 @@
+﻿const userService = require('./service');
+
+module.exports.handler = ((event, context, callback) => {
+    userService.getAll()
+        .then(users => {
+            callback(null, users)
+        })
+        .catch(err => {
+            console.log('Error', err)
+            callback(err)
+        });
+});
---
+++ users/service.js
@@ -0,0 +1,42 @@
+﻿//const data = require('../_helpers/data')
+//const users = data.users
+let nextId = 6
+
+module.exports = {
+    //authenticate,
+    getAll,
+    //register
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
+
+async function getAll() {
+    return {};
+    // return users.map(u => {
+    //     const { password, ...userWithoutPassword } = u;
+    //     return userWithoutPassword;
+    // });
+}
+
+async function register({ username, password, firstName, lastName }) {
+    const newUser = {
+        id: nextId,
+        username,
+        password,
+        firstName,
+        lastName,
+        role: 'member'
+    }
+
+    users.push(newUser)
+    nextId++
+
+    const { pass, ...userWithoutPassword } = newUser;
+    return userWithoutPassword;
+}
