24f8ea8c0b7c55ae5b2c5becd5bd183189812e92
Jojo <katjajohanna@outlook.com>
2019-10-17 20:36:24 +0300

    Add function to update hour

---
+++ hours/put-handler.js
@@ -0,0 +1,19 @@
+﻿const hourService = require('./service')
+
+module.exports.handler = ((event, context, callback) => {
+    console.log(event)
+
+    hourService.updateHour(event.pathParameters.id, JSON.parse(event.body))
+    .then(hour => {
+        console.log('Updated hour', hour)
+        callback(null, {
+            "statusCode": 200,
+            "headers": {},
+            "body": JSON.stringify(hour)
+        })
+    })
+    .catch(err => {
+        console.log('Error', err)
+        callback(err)
+    })
+});
--- hours/service.js
+++ hours/service.js
@@ -1,5 +1,8 @@
 const AWS = require('aws-sdk')
-const dynamo = new AWS.DynamoDB.DocumentClient({ region: process.env.AWS_REGION })
+const dynamo = new AWS.DynamoDB.DocumentClient({
+    region: process.env.AWS_REGION,
+    apiVersion: '2012-08-10'
+})
 
 async function getAll() {
     return dynamo.scan({
@@ -15,6 +18,47 @@ async function getAll() {
     })
 }
 
+async function updateHour(id, { projectId, hours, description }) {
+    let updateExpressions = [];
+    let expressionAttributeValues = {}
+
+    if (projectId != undefined) {
+        updateExpressions.push('projectId = :pid')
+        expressionAttributeValues[":pid"] = projectId
+    }
+
+    if (hours != undefined) {
+        updateExpressions.push('hours = :h')
+        expressionAttributeValues[":h"] = hours
+    }
+
+    if (description != undefined) {
+        updateExpressions.push('description = :desc')
+        expressionAttributeValues[":desc"] = description
+    }
+
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE_HOURS,
+        Key: { id },
+        ExpressionAttributeValues: expressionAttributeValues,
+        UpdateExpression: 'SET ' + updateExpressions.join(', ')
+    }
+
+    console.log(params)
+
+    return dynamo.update(params)
+        .promise()
+        .then(hour => {
+            console.log('Success', hour)
+            return hour // TODO fetch updated hour. This is empty object
+        })
+        .catch(err => {
+            console.log('Error', err)
+            return {}
+        })
+}
+
 module.exports = {
-    getAll
+    getAll,
+    updateHour
 }
